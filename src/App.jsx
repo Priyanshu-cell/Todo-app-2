7cb8b9ca88ddbe95bc894515b074ff6017612166
Priyanshu Maithani <priyanshumaithani02@gmail.com>
2024-06-21 16:39:21 +0530

    created diff diff components for indivisual items

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import AppName from "./components/AppName";
 import AppTodo from "./components/AddTodo";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
+import TodoItems from "./components/TodoItems";
 import "./App.css";
 
 function App() {
+  const todoItems = [
+    {
+      name: "Buy milk",
+      dueDate: "15/05/24",
+    },
+    {
+      name: "Go to office",
+      dueDate: "15/05/24",
+    },
+  ];
+
   return (
-    <center>
+    <center className="todo-container">
       <AppName />
-      <AppTodo></AppTodo>
-      <div className="item-container">
-        <TodoItem1 />
-        <TodoItem2 />
-      </div>
+      <AppTodo />
+      <TodoItems todoItems={todoItems}></TodoItems>
     </center>
   );
 }
- 
-export default App; 
+
+export default App;
